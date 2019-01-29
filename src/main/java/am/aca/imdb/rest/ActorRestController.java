58fadca46faa1bcf830c0cb3f3393d0cb5aca752
Sargis <saqogh1993@gmail.com>
2019-01-29 22:46:58 +0400

    after merging to gar_imdb, rest are working, just movies are not getting with all relations

--- src/main/java/am/aca/imdb/rest/ActorRestController.java
+++ src/main/java/am/aca/imdb/rest/ActorRestController.java
@@ -26,10 +26,12 @@ public class ActorRestController {
     public List<Actor> findAllActors() {
         return actorDao.findAll();
     }
+
     @GetMapping("/actors/{id}")
     public Actor findByActorId(@PathVariable(value = "id") long id){
         return actorDAO.findOne(id);
     }
+
     @GetMapping("/actors/movies/{id}")
     public Set<Movie> findMoviesByActorId(@PathVariable(value = "id") long id){
         Set<Movie> movies = actorDAO.findOne(id).getMovies();
